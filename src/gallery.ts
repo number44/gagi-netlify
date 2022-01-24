d2b4515a269ce9181ba5353bcee010ead6aaccfb
number44 <eldan8600@gmail.com>
2022-01-24 18:00:42 +0100

    publish

--- src/gallery.ts
+++ src/gallery.ts
@@ -1,8 +1,15 @@
 const gallery = () => {
 	const close = document.querySelector('.close-slideshow') as HTMLDivElement;
 	const slideshow = document.querySelector('.slideshow') as HTMLDivElement;
+	const thumbs = document.querySelectorAll('.gallery-thumb');
 	close?.addEventListener('click', () => {
 		slideshow.classList.add('d-none');
+	});
+
+	thumbs.forEach((t) => {
+		t.addEventListener('click', () => {
+			slideshow.classList.remove('d-none');
+		});
 	});
 };
 export default gallery;
